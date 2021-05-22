81b8a927a2bcfe6d172414a358b321c0e5bc0b33
Salim Mayan <mailsalim@gmail.com>
2021-05-22 02:25:11 -0700

    add home controller with Index view

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,6 +5,6 @@ namespace HairSalon.Models
         public int ClientId { get; }                //1st coloumn in "clients" table: primary key in table (NOT used as foreign key in Stylist.cs)
         public string Description { get; set; }     //2nd coloumn in "clients" table
         public int StylistId { get; set; }          //3rd coloumn in "clients" table: primary key in "stylists" table used as as foreign key in "clients" table
-        public virtual Stylist Stylist { get; set; }
+        public virtual Stylist Stylist { get; set; }  //NOT a column in table: To establish r/s have also added a "virtual" Many property (object of Many)
     }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -8,7 +8,6 @@ namespace HairSalon.Models
     {
       this.Clients = new HashSet<Client>(); //HashSet is an unordered collection of unique elements - no duplicates allowed (one to many)/handles no records case
     }
-
     public int StylistId { get; set; }  //1st coloumn in "stylists" table: primary key in table (used as foreign key in Client.cs)
     public string Name { get; set; }  //2nd column in "stylists" table
     public virtual ICollection<Client> Clients { get; set; }  //"Clients" represent DBSet of "one" in "many". 
