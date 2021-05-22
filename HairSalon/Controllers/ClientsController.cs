509b0b0d4c01144d9690063a9354b0f8c66d662e
Salim Mayan <mailsalim@gmail.com>
2021-05-22 03:10:41 -0700

    fix missing get in autoproperty inside Client.cs

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -7,33 +7,34 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-    public class ClientsController : Controller
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
     {
-        private readonly HairSalonContext _db;
-
-        public ClientsController(HairSalonContext db)
-        {
-            _db = db;
-        }
-
-        public ActionResult Index()
-        {
-            List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
-            return View(model);
-        }
-        // public ActionResult Create()
-        // {
-        //     ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
-        //     return View();
-        // }
-
-        // [HttpPost]
-        // public ActionResult Create(Client client)
-        // {
-        //     _db.Clients.Add(client);
-        //     _db.SaveChanges();
-        //     return RedirectToAction("Index");
-        // }
-
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+  }
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,7 +2,7 @@ namespace HairSalon.Models
 {
     public class Client
     {
-        public int ClientId { get; }                //1st coloumn in "clients" table: primary key in table (NOT used as foreign key in Stylist.cs)
+        public int ClientId { get; set; }                //1st coloumn in "clients" table: primary key in table (NOT used as foreign key in Stylist.cs)
         public string Description { get; set; }     //2nd coloumn in "clients" table
         public int StylistId { get; set; }          //3rd coloumn in "clients" table: primary key in "stylists" table used as as foreign key in "clients" table
         public virtual Stylist Stylist { get; set; }  //NOT a column in table: To establish r/s have also added a "virtual" Many property (object of Many)
