054ec6746eacf58890a5c87b5f5d683d70c75c08
Salim Mayan <mailsalim@gmail.com>
2021-05-22 01:00:15 -0700

    update Stylist.cs

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,13 +1,16 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
-    public class Stylist
+  public class Stylist
+  {
+    public Stylist()
     {
-        public string Name {get; set; }
-        public int StylistId {get;}
-        // public List<string> Clients = 
-        public Stylist(string name){
-            Name = name;
-        }
-
+      this.Clients = new HashSet<Client>();
     }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
 }
